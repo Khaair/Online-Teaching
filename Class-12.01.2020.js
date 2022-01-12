895c1f1b2b72e2e3fea5359b4c357ddc7b510ae1
Khaair <khair.cse.iu@gmail.com>
2022-01-12 16:03:16 +0600

    Khair

---
+++ Class-12.01.2020.js
@@ -0,0 +1,75 @@
+
+
+for(let i = 1; i <= 10; i++){
+  console.log(i)
+}
+
+
+
+
+let sum = 0
+
+for(let i = 1; i <= 3; i++){
+  sum = sum + i**2
+}
+
+console.log(sum)
+
+
+
+1^2 + 2^2 + 3^2+....+n
+
+
+
+
+let sum = 0
+
+for(let i = 1; i <= 10; i++){
+  
+  sum = sum +i
+ 
+}
+
+ console.log(sum)
+ 
+ 
+//  1+3+5+......+n
+
+let num = 0;
+for(let i = 1; i<=10;i = i + 2){
+  num = num + i
+}
+console.log(num)
+
+
+
+
+// Area of a triangle
+
+let a = 5
+let b = 4
+let c = 3
+
+let s = (a+b+c)/2
+
+let z = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+
+
+console.log(z)
+
+
+
+let x = [1, 3, 5]
+
+let arr = [2, 4, 6, 8]
+
+
+
+let z = x.concat(arr)
+
+console.log(z)
+
+
+
+

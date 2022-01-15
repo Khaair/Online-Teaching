f2b2d0774c89c35f757a989123fb7c2c25b79aa5
Khaair <khair.cse.iu@gmail.com>
2022-01-15 20:13:14 +0600

    Update

---
+++ Class 15.01.22.js
@@ -0,0 +1,18 @@
+// Find Factorial Number
+
+function factorial(a){
+  
+    let s = 1
+    
+    for(let i = a; i >= 1;i--){
+      
+      s = s *i  
+    }
+     return s
+   
+  }
+  
+  console.log(factorial(5))
+  
+  // 5*4*3*2*1
+  

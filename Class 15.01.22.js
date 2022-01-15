f0752d9dbb274515f03d89565e5ba2fb263cfd99
Khaair <khair.cse.iu@gmail.com>
2022-01-15 20:15:28 +0600

    Update

--- Class 15.01.22.js
+++ Class 15.01.22.js
@@ -29,3 +29,23 @@ function factorial(a){
   }
   
   console.log(res(190))
+  
+  // Raf
+  
+  // let a = -1;
+  // let sam = (a > 0) ? (a == 0 ? "zero" : "positive") : "negative";
+  // console.log(sam);
+  
+  // function number(a){
+  //   var x=(a>0) ? "positive":"Nagative"
+  //   return x;
+  // }
+  // var result=number(10)
+  // console.log(result)
+  
+  
+  // function number(a){
+  //   var x=(a>0) ? "positive":"Nagative"
+  //   console.log(x)
+  // }
+  // var result=number(-10)
